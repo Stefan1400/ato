201eef9754e9201b496c5abf8a643c90adca1244
Stefan1400 <stefan.besonen783@gmail.com>
2026-02-26 10:07:52 -0700

    cleanup(feedback): improve and shorten feedback messaging for better UX

--- server/src/services/feedbackService.js
+++ server/src/services/feedbackService.js
@@ -60,26 +60,26 @@ const feedbackService = async (userId) => {
    // both sessions from Y & T
    if (yesterday.count > 0 && today.count > 0) {
       if (today.totalMs > yesterday.totalMs) {
-         return `Your total focused time today is ${today.total} compared to yesterday's focused time (${yesterday.total}). Heck yeah!`
+         return `You focused for ${today.total} today vs (${yesterday.total}) yesterday. Heck yeah!`
       } else if (today.totalMs === yesterday.totalMs) {
-         return `That's a first. Your total focused time today (${today.total}) is tied with your total focused time yesterday (${yesterday.total}). Wanna beat it?`;
+         return `You matched yesterday at ${yesterday.total}. Wanna beat it?`;
       } else if (today.longestMs > yesterday.longestMs) {
-         return `Your longest session today is ${today.longest} compared to yesterday's longest session (${yesterday.longest}). Awesome job!`
+         return `Your longest session today was ${today.longest} vs yesterday (${yesterday.longest}). Awesome job!`
       } else if (today.averageMs > yesterday.averageMs) {
-         return `Your average session time today is ${today.average} compared to yesterday's average session time (${yesterday.average}). Great work!`
+         return `Your average session today was ${today.average} vs yesterday (${yesterday.average}). Great work!`
       } else {
-         return `Your total focused time today is ${today.total}. Keep it up!`;
+         return `Your focus time today is ${today.total}. Keep it up!`;
       }
    } 
    
    // sessions from T but none from Y
    else if (yesterday.count === 0 && today.count > 0) {
-      return `Your total focused time today is ${today.total}. Let's keep it up!`;
+      return `Your focus time today is ${today.total}. Let's keep it up!`;
    }
    
    // sessions from Y but none from T
    else if (yesterday.count > 0 && today.count === 0) {
-      return `Your total focused time yesterday was ${yesterday.total}. Let's hit it even harder today!`;
+      return `Your focus time yesterday was ${yesterday.total}. Let's hit it even harder today!`;
    } 
    
    // no sessions from either Y or T
