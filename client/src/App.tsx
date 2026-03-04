86920ae4a9216eb69edf5b0cf7c2bae2e3fca3e0
Stefan1400 <stefan.besonen783@gmail.com>
2026-03-04 09:36:01 -0700

    chore(css): refine global styles and Tailwind setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <div className="min-h-scree text-white flex items-center justify-center bg-blue-500">
+    <div className="h-5 w-full bg-red-500 text-black">
       <h1 className="text-4xl font-bold">
         Tailwind v4 is working 🚀
       </h1>
