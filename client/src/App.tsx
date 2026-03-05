7e193d9ed3dfef34ad2c52aec0bcf95b21cf0c60
Stefan1400 <stefan.besonen783@gmail.com>
2026-03-05 10:06:08 -0700

    feat(auth): implement sign up page UI (WIP)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
+import RegisterPage from "./features/auth/RegisterPage"
+
 export default function App() {
   return (
-    <div className="h-5 w-full bg-red-500 text-black">
-      <h1 className="text-4xl font-bold">
-        Tailwind v4 is working 🚀
-      </h1>
+    <div className="">
+      <RegisterPage />
     </div>
   )
 }
