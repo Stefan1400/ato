f97e87c8f26ed6e0e2bc5deaaff8be0eacdcc326
Stefan1400 <stefan.besonen783@gmail.com>
2026-03-08 10:35:59 -0700

    feat(homepage): setup homepage routing

--- client/src/app/router.tsx
+++ client/src/app/router.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import RegisterPage from "../features/auth/RegisterPage";
 import LoginPage from "../features/auth/LoginPage";
+import HomePage from "../pages/Homepage";
 
 export function AppRouter() {
    return (
       <Routes>
+         <Route path="/" element={<HomePage />} />
          <Route path="/signup" element={<RegisterPage />} />
          <Route path="login" element={<LoginPage />} />
       </Routes>
---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div className='w-screen h-screen bg-[#151515]'>
+
+    </div>
+  )
+}
+
+export default HomePage;
