eb7ecff4db90cfe8db143bd89cebd68460569d13
Stefan1400 <stefan.besonen783@gmail.com>
2026-03-09 11:34:00 -0700

    style(session-timer): remove subheader and improve styling

--- client/src/features/tracker/SessionTimer.tsx
+++ client/src/features/tracker/SessionTimer.tsx
@@ -45,16 +45,15 @@ function SessionTimer() {
    };
 
    return (
-    <div className={`${timerStatus === 'finished' ? 'bg-[#0AD000] text-white' : timerStatus === 'ongoing' ? 'bg-[#D00000] text-white' : 'bg-[#1F1F1F] text-white'} w-full h-auto border-2 border-[#2A2A2A] flex flex-row justify-between items-center p-3 rounded-md`}>
-      <div className='flex flex-col items-start justify-between gap-2'>
-         <h2 className="font-normal">Start your session</h2>
-         <span onClick={startTimer} className="text-3xl font-semibold">{formatTime(time)}</span>
-         <h3 className={`font-normal ${timerStatus === 'default' ? 'text-[#7E7E7E]' : 'text-white'}`}>{timerStatus === 'finished' ? 'session ended' : 'current session'}</h3>
+    <div className={`${timerStatus === 'finished' ? 'bg-[#04724D] text-white' : timerStatus === 'ongoing' ? 'bg-[#FF9F1C] text-black' : 'bg-[#1F1F1F] text-white'} w-full h-auto border-2 border-[#2A2A2A] flex flex-row justify-between items-center rounded-md min-h-[96px] pl-3`}>
+      <div className='flex flex-col items-start'>
+         <h2 className="font-semibold text-md">{timerStatus === 'ongoing' ? 'Focus Session' : timerStatus === 'finished' ? 'Session Complete' : 'Ready'}</h2>
+         <span onClick={startTimer} className="text-[2rem] font-bold">{formatTime(time)}</span>
       </div>
 
       {timerStatus !== 'finished' && (
          <button onClick={handleClick} className="p-3.5">
-            <div className='p-6 bg-[#161616] rounded-full border-3 border-[#2A2A2A]'>
+            <div className='p-5 bg-[#0C0C0C] rounded-full border-3 border-[#2A2A2A]'>
                {timerStatus === 'default' && (
                   <svg width="20" height="21" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                      <path d="M2.41923 0.197138C1.9125 -0.076068 1.29952 -0.0640149 0.800961 0.225262C0.302404 0.514539 0 1.04086 0 1.60736V16.3926C0 16.9591 0.30649 17.4855 0.800961 17.7747C1.29543 18.064 1.9125 18.0761 2.41923 17.8029L16.15 10.4102C16.6731 10.129 17 9.58659 17 9C17 8.41341 16.6731 7.87102 16.15 7.58977L2.41923 0.197138Z" fill="white"/>
