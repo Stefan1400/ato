72ca9e10752dbdbb5ed67cb0a1bf414b1fa71213
Stefan1400 <stefan.besonen783@gmail.com>
2026-03-16 10:47:39 -0700

    feat(feedback): add basic FeedbackMessage component layout

---
+++ client/src/features/feedback/feedbackMessage.tsx
@@ -0,0 +1,21 @@
+function FeedbackMessage() {
+  return (
+    <div>
+      <h2>Today's Focus Insight</h2>
+      <div>
+         <div>
+            <span>Today</span>
+            <span>2h 10min</span>
+         </div>
+         <div>
+            <span>Yesterday</span>
+            <span>2h 10min</span>
+         </div>
+      </div>
+
+      <p>message</p>
+    </div>
+  )
+}
+
+export default FeedbackMessage;
