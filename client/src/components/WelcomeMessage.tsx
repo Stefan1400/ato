80c7ccb3b5553f72dabe9f34f10a9afdbeb4ca2e
Stefan1400 <stefan.besonen783@gmail.com>
2026-03-17 12:27:13 -0700

    feat(homepage): add welcome message UI

---
+++ client/src/components/WelcomeMessage.tsx
@@ -0,0 +1,10 @@
+function WelcomeMessage() {
+  return (
+    <div className="w-screen h-auto p-3 pl-7 flex flex-col items-start text-white gap-1 mb-11 mt-5">
+      <h1 className="font-bold text-2xl">Good morning, Stefan</h1>
+      <p>Ready to get started?</p>
+    </div>
+  )
+}
+
+export default WelcomeMessage
