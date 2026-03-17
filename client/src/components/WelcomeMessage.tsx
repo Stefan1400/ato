a0a032805e2389439c8982b8837ca25f28ab7cc6
Stefan1400 <stefan.besonen783@gmail.com>
2026-03-17 13:50:32 -0700

    feat(welcome): add dynamic greeting based on time of day

--- client/src/components/WelcomeMessage.tsx
+++ client/src/components/WelcomeMessage.tsx
@@ -1,27 +1,45 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../app/AuthProvider";
 import type { AuthContextType } from "../app/AuthProvider";
 import { useGetFeedback } from "../features/feedback/useFeedback";
+
+type DayTimes = 'Good morning' | 'Good afternoon' | 'Good evening';
 
 function WelcomeMessage() {
 
    const { user } = useContext(AuthContext) as AuthContextType;
    const feedbackMutation = useGetFeedback();
-
-   function handleGetFeedback() {
-      feedbackMutation.mutate();
-   };
+   const [greeting, setGreeting] = useState<DayTimes>();
 
    const today = feedbackMutation.data?.todayValue;
    
    const username = user?.email.split('@')[0];
    const displayName = username ? username[0].toUpperCase() + username.slice(1) : 'Guest'
 
+   function getTimeOfDay() {
+      const hours = new Date().getHours();
+
+      if (hours >= 0 && hours < 12) return 'Good morning';
+      if (hours >= 12 && hours < 18) return 'Good afternoon';
+      return 'Good evening';
+   };
+
+   function handleGetFeedback() {
+      feedbackMutation.mutate();
+   };
+
+   useEffect(() => {
+      setGreeting(getTimeOfDay())
+      
+      const interval = setInterval(() => setGreeting(getTimeOfDay()), 60 * 1000);
+      return () => clearInterval(interval);
+   }, []);
+
   return (
     <div className="w-screen h-auto p-3 pl-7 flex flex-col items-start text-white gap-1 mb-11 mt-5">
       <button onClick={handleGetFeedback}>get feedback</button>
       
-      <h1 className="font-bold text-2xl">Good morning, {displayName}</h1>
+      <h1 className="font-bold text-2xl">{greeting}, {displayName}</h1>
       <p className="text-[#c3c3c3]">
          {!today ? (
          'Ready to get started? 😊'
