af18ec1756b876b00536439e51bfc80af6dbd3f1
Stefan1400 <stefan.besonen783@gmail.com>
2026-03-17 13:16:05 -0700

    feat(welcome): make WelcomeMessage dynamic with user and feedback data

--- client/src/components/WelcomeMessage.tsx
+++ client/src/components/WelcomeMessage.tsx
@@ -1,10 +1,38 @@
+import { useContext } from "react";
+import { AuthContext } from "../app/AuthProvider";
+import type { AuthContextType } from "../app/AuthProvider";
+import { useGetFeedback } from "../features/feedback/useFeedback";
+
 function WelcomeMessage() {
+
+   const { user } = useContext(AuthContext) as AuthContextType;
+   const feedbackMutation = useGetFeedback();
+
+   function handleGetFeedback() {
+      feedbackMutation.mutate();
+   };
+
+   const today = feedbackMutation.data?.todayValue;
+   
+   const username = user?.email.split('@')[0];
+   const displayName = username ? username[0].toUpperCase() + username.slice(1) : 'Guest'
+
   return (
     <div className="w-screen h-auto p-3 pl-7 flex flex-col items-start text-white gap-1 mb-11 mt-5">
-      <h1 className="font-bold text-2xl">Good morning, Stefan</h1>
-      <p>Ready to get started?</p>
+      <button onClick={handleGetFeedback}>get feedback</button>
+      
+      <h1 className="font-bold text-2xl">Good morning, {displayName}</h1>
+      <p className="text-[#c3c3c3]">
+         {!today ? (
+         'Ready to get started? 😊'
+         ) : (
+         <>
+            You've focused <span className="text-white font-medium">{today} 💪</span> today. Ready to continue?
+         </>
+         )}
+      </p>
     </div>
   )
-}
+};
 
-export default WelcomeMessage
+export default WelcomeMessage;
