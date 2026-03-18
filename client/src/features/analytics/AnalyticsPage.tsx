2c659f8751bbef93e82a2a617df820616b2df4d8
Stefan1400 <stefan.besonen783@gmail.com>
2026-03-18 09:14:54 -0700

    feat(analytics): add analytics page UI with FeedbackMessage component

---
+++ client/src/features/analytics/AnalyticsPage.tsx
@@ -0,0 +1,11 @@
+import FeedbackMessage from "../feedback/feedbackMessage";
+
+function AnalyticsPage() {
+  return (
+    <div className='w-screen h-screen bg-[#151515] flex flex-col items-center px-6 pt-21'>
+      <FeedbackMessage />
+    </div>
+  )
+};
+
+export default AnalyticsPage;
