8fb7c063ca4f7e2500e6eea5baa11ae78d672eea
Stefan1400 <stefan.besonen783@gmail.com>
2026-03-18 11:32:33 -0700

    feat(feedback): improve FeedbackComponent styling

--- client/src/features/feedback/feedbackMessage.tsx
+++ client/src/features/feedback/feedbackMessage.tsx
@@ -5,25 +5,15 @@ import { useGetFeedback } from "./useFeedback";
 function FeedbackMessage({ }) {
    const { data, isLoading, isError } = useGetFeedback();
 
-   const feedbackType = data?.feedbackType;
+   let feedbackType = data?.feedbackType;
    const today = data?.todayValue || 0;
    const yesterday = data?.yesterdayValue || 0;
   
    return (
-    <div className="w-screen h-auto flex flex-col items-start justify-start absolute bottom-0 bg-[#2A2A2A] text-white gap-2">
-      <h2>Today's Focus Insight</h2>
-      <div>
-         <div className="flex flex-row gap-3">
-            <span className="font-semibold">today:</span>
-            <span className="text-green-500">{today === 0 ? today+"min" : today}</span>
-         </div>
-         <div className="flex flex-row gap-3">
-            <span className="font-semibold">Yesterday:</span>
-            <span className="text-green-500">{yesterday === 0 ? yesterday+"min" : yesterday}</span>
-         </div>
-      </div>
-
-      <p>message</p>
+    <div className="w-screen h-auto flex flex-col items-start justify-start text-white gap-2 p-4 pl-5">
+      <p className="text-[#474747] font-medium">Today</p>
+      <h1 className="text-4xl">{!today ? '0min' : today}</h1>
+      <p className="text-[#a8a8a8] w-70">message</p>
     </div>
   )
 }
