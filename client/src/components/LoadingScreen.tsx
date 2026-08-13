c5edb751df0d72f4403ece4974b10be551c49578
Stefan1400 <stefan.besonen783@gmail.com>
2026-08-12 17:06:39 -0700

    style(loading-screen): change background color

--- client/src/components/LoadingScreen.tsx
+++ client/src/components/LoadingScreen.tsx
@@ -6,7 +6,7 @@ type LoadingScreenTypes = {
 
 function LoadingScreen({ text }: LoadingScreenTypes) {
   return (
-    <div className="w-screen h-screen bg-[#151515] text-white flex flex-col items-center justify-center z-1200 absolute left-0 top-0 gap-5">
+    <div className="w-screen h-screen bg-[#090909] text-white flex flex-col items-center justify-center z-1200 absolute left-0 top-0 gap-5">
       <Loader className="animate-spin [animation-duration:1.4s] w-8 h-8" />
       <h1 className="text-2xl">{text}</h1>
     </div>
